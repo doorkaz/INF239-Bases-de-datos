c8aff93e7a9ca570c2f74d138e065bc72778fab9
GabrielLeight <107009481+GabrielLeight@users.noreply.github.com>
2023-05-22 23:10:46 -0400

    Add files via upload

---
+++ mitarea/Prestigetravels/prestigetravels.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-05-2023 a las 05:09:45
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prestigetravels`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hoteles`
+--
+
+CREATE TABLE `hoteles` (
+  `id_hotel` int(11) NOT NULL,
+  `Num_estrellas` int(11) NOT NULL,
+  `Precio_noche` int(11) NOT NULL,
+  `Ciudad` varchar(200) NOT NULL,
+  `cant_total_hab` int(11) NOT NULL,
+  `hab_disp` int(11) NOT NULL,
+  `estacionamiento` tinyint(1) NOT NULL,
+  `piscina` tinyint(1) NOT NULL,
+  `serv_lavanderia` tinyint(1) NOT NULL,
+  `pet_friend` tinyint(1) NOT NULL,
+  `serv_desayuno` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paquetes`
+--
+
+CREATE TABLE `paquetes` (
+  `id_pack` int(11) NOT NULL,
+  `aero_ida` varchar(200) NOT NULL,
+  `aero_vuelta` text NOT NULL,
+  `ciudades` text NOT NULL,
+  `hospedajes` text NOT NULL,
+  `fecha_salida` date NOT NULL,
+  `fecha_llegada` date NOT NULL,
+  `total_noches` int(11) NOT NULL,
+  `precio_persona` int(11) NOT NULL,
+  `cant_pack_disp` int(11) NOT NULL,
+  `total_packs` int(11) NOT NULL,
+  `total_person_pack` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resena_hotel`
+--
+
+CREATE TABLE `resena_hotel` (
+  `id_hotel` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `limpieza` int(11) NOT NULL,
+  `servicio` int(11) NOT NULL,
+  `decoracion` int(11) NOT NULL,
+  `Calidad_cama` int(11) NOT NULL,
+  `texto_resena` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resena_pack`
+--
+
+CREATE TABLE `resena_pack` (
+  `id_pack` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `calidad_hoteles` int(11) NOT NULL,
+  `transporte` int(11) NOT NULL,
+  `servicio` int(11) NOT NULL,
+  `rel_cal_precio` int(11) NOT NULL,
+  `texto_resena` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `Correo` varchar(200) NOT NULL,
+  `Nombre` varchar(200) NOT NULL,
+  `Fecha_Nacimiento` date NOT NULL,
+  `Contrasena` varchar(30) NOT NULL,
+  `wishlist` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `hoteles`
+--
+ALTER TABLE `hoteles`
+  ADD PRIMARY KEY (`id_hotel`);
+
+--
+-- Indices de la tabla `paquetes`
+--
+ALTER TABLE `paquetes`
+  ADD PRIMARY KEY (`id_pack`);
+
+--
+-- Indices de la tabla `resena_hotel`
+--
+ALTER TABLE `resena_hotel`
+  ADD PRIMARY KEY (`id_hotel`,`id_usuario`),
+  ADD KEY `id_hotel` (`id_hotel`,`id_usuario`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `resena_pack`
+--
+ALTER TABLE `resena_pack`
+  ADD PRIMARY KEY (`id_pack`,`id_usuario`),
+  ADD KEY `id_pack` (`id_pack`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `hoteles`
+--
+ALTER TABLE `hoteles`
+  MODIFY `id_hotel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `paquetes`
+--
+ALTER TABLE `paquetes`
+  MODIFY `id_pack` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `resena_hotel`
+--
+ALTER TABLE `resena_hotel`
+  ADD CONSTRAINT `resena_hotel_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `resena_hotel_ibfk_2` FOREIGN KEY (`id_hotel`) REFERENCES `hoteles` (`id_hotel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `resena_pack`
+--
+ALTER TABLE `resena_pack`
+  ADD CONSTRAINT `resena_pack_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `resena_pack_ibfk_2` FOREIGN KEY (`id_pack`) REFERENCES `paquetes` (`id_pack`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
