fc87c5079568ceed1d1e4e175ed93776e1d1a1d0
GabrielLeight <gabriel.leyton@usm.cl>
2023-06-05 20:46:56 -0400

    new file:   mitarea/PHP/cart.php 	modified:   mitarea/PHP/wishlist2.php

--- mitarea/PHP/wishlist2.php
+++ mitarea/PHP/wishlist2.php
@@ -1,18 +1,13 @@
 <?php
 include "db_conn.php";  
-$result = mysqli_query($conn,"DROP PROCEDURE IF EXISTS add_wishlist;");
-$query = "CREATE PROCEDURE add_wishlist(IN val VARCHAR(200),IN id int(11)) 
+$result = mysqli_query($conn,"DROP PROCEDURE IF EXISTS leave_wishlist;");
+$query = "CREATE PROCEDURE leave_wishlist(IN productid int(11)) 
 BEGIN 
-    UPDATE usuarios
-    SET wishlist=(
-        CASE WHEN wishlist=''
-            THEN val
-            ELSE concat_WS('\,',wishlist, val)
-        END
-    )
-    WHERE id_usuario = id;
+    DELETE FROM wishlist
+    WHERE pid = productid;
 END;";
 $result = mysqli_query($conn,$query) 
     // str_replace($search, $replace, $subject)
+
 
 ?>
