8f7ebc840361839b5445f3cf9ba96cbb4aab2d61
GabrielLeight <gabriel.leyton@usm.cl>
2023-06-06 19:50:10 -0400

    new file:   mitarea/PHP/Detalles.php 	modified:   mitarea/PHP/Navbar.php 	modified:   mitarea/PHP/hoteles.php

--- mitarea/PHP/hoteles.php
+++ mitarea/PHP/hoteles.php
@@ -81,7 +81,7 @@
                                            
                                             echo '<input type="hidden"  value="'.$hotel['id_hotel'].'" name= "pid">'; 
                                             echo '<input type="hidden"  value="0" name= "bool">'; 
-                                            echo '<button type = "submit" class="btn btn-reserve rounded">Agregar al carrito</button>';  
+                                            echo '<button type = "submit" name="cart" class="btn btn-reserve rounded">Agregar al carrito</button>';  
                                             echo '<button type="submit" name="wish" class="btn btn-cart rounded mt-1">Wishlist</button>';
                                             
                                         echo '</div>';
@@ -94,6 +94,9 @@
             }
             if (ISSET($_POST['wish'])){
                 include 'wishlist.php'; 
+            }
+            if (ISSET($_POST['cart'])){
+                include 'cart.php'; 
             }
             ?>
             
