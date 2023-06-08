d741aa55019686fdce51913e09cbe0850516c385
doorkaz <retamalespj@gmail.com>
2023-06-07 20:59:44 -0400

    cart

--- mitarea/PHP/mycart.php
+++ mitarea/PHP/mycart.php
@@ -21,6 +21,7 @@
 	<link rel = "stylesheet" type = "text/css" href = "../css/navbar.css">
 	<link rel = "stylesheet" type = "text/css" href = "../css/table.css">
 	<link rel = "stylesheet" type = "text/css" href = "../css/general.css">
+	
 	<!--Jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<!-- FONTAWESOME -->
@@ -30,8 +31,6 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 	<!-- BOOTSTRAP FONT ICON -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-	<!-- ajax -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -42,12 +41,8 @@
     <div class="container"> 
     
         <h2 class='text-center text-white'>Cart</h2>
-
-        
+   
             <?php include "show-cart.php"; ?>
-        
-            
-       
 
     </div>
 	
